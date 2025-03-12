ebb18d92f1dfd26c597446331ce8bc53a8cecf9d
Rakesh Jain <rakeshvora2007@gmail.com>
2025-03-12 03:05:29 -0400

    temporary data response

---
+++ data/education.js
@@ -0,0 +1,67 @@
+const education = [
+      {
+        "educationImage": "https://firebasestorage.googleapis.com/v0/b/personal-website-f8d5d.appspot.com/o/images%2Fimg-1588183800124?alt=media&token=883817b4-874c-4450-8251-156d1318a3e2",
+        "name": "California State University, Fullerton",
+        "degree": "Master's degree, Computer Engineering",
+        "period": "2016 - 2017",
+        "gpa": "3.7",
+        "activities": [
+          "SASA (South Asian Student Association) - Member",
+          "International Program and Global Engagement - Volunteer"
+        ],
+        "courseWork": [
+          "Advance Hardware Security Design",
+          "Low Power Digital IC Design",
+          "Java Back-End Engineering for Enterprise Application",
+          "Operating System Design"
+        ]
+      },
+      {
+        "educationImage": "https://firebasestorage.googleapis.com/v0/b/personal-website-f8d5d.appspot.com/o/images%2Fimg-1588184520960?alt=media&token=02cc2d67-e1d0-4462-bce0-7e4205470713",
+        "name": "University of Mumbai",
+        "degree": "Bachelor’s Degree, Information Technology",
+        "period": "2011 - 2015",
+        "activities": [
+          "ACM (Association for Computing Machinery) - Event Manager"
+        ],
+        "courseWork": [
+          "Communication Skills",
+          "Computational Mathematics",
+          "Computer Graphics and Virtual Reality Systems",
+          "Computer Programming",
+          "Convergence of Technologies and Networking in Communication",
+          "Data Structure and Algorithm",
+          "Data warehousing, Mining, and Business Intelligence",
+          "Database Technologies",
+          "Digital Logic Design and Applications",
+          "Digital Signal and Image Processing",
+          "Engineering Drawing",
+          "Environmental Studies",
+          "Financial Accounting and Management of Technology Innovation",
+          "GUI and Database Management",
+          "Gaming Architecture and Programming",
+          "Information Storage Management and Disaster Recovery",
+          "Information Technology for Management of Enterprise",
+          "Information and Network Security",
+          "Internet Programming",
+          "Microprocessor and Microcontrollers",
+          "Middleware Enterprise Integration Technologies",
+          "Mobile and E-commerce",
+          "Network Technology for Digital Devices",
+          "Object Oriented Analysis and Design",
+          "Open Source Software Laboratory",
+          "Operating System for Computational Devices",
+          "Presentation and Communication Techniques",
+          "Principle of Communication Engineering",
+          "Programming for Mobile and Remote Computers",
+          "Simulation and Modeling",
+          "Software Engineering",
+          "Software Project Management",
+          "Software Testing and Quality Assurance",
+          "Wireless Network"
+        ]
+      }
+    ];
+  
+
+module.exports = education;
---
+++ data/work_experience.js
@@ -0,0 +1,86 @@
+const work_experience = {
+    "data": [
+      {
+        "companyName": "Prime Software Solution",
+        "workImage": "https://firebasestorage.googleapis.com/v0/b/personal-website-f8d5d.appspot.com/o/images%2Fimg-1588187632701?alt=media&token=eef0bdc9-dc4d-4f3d-82ec-1fd7175c1a2e",
+        "designation": "Software Developer",
+        "location": "Mumbai Area, India",
+        "duration": "Jun 2014 - Jan 2016 · 1 yr 8 mos",
+        "responsibilities": [
+          "Designed, developed and maintained client’s site front end functionality using HTML, CSS, JavaScript.",
+          "Built Android applications on Android Studio using Android Java and XML.",
+          "Developed RESTful web services for multi-user applications.",
+          "Created complex SQL queries and used JDBC for database connectivity.",
+          "Developed lightweight applications to export files in PDF, XML, and CSV to generate bills and ledgers."
+        ]
+      },
+      {
+        "companyName": "AutoXplorer",
+        "workImage": "https://firebasestorage.googleapis.com/v0/b/personal-website-f8d5d.appspot.com/o/images%2Fimg-1588187763954?alt=media&token=3ff94c16-ae96-4fa3-af36-31ed90221be3",
+        "designation": "Software Engineer",
+        "location": "Irvine, California, United States",
+        "duration": "Aug 2017 - Dec 2017 · 5 mos",
+        "responsibilities": [
+          "Developed an auto dealer management software using React, Redux, and Node.js.",
+          "Implemented the Web Front End based on JavaScript, JQuery and HTML/CSS.",
+          "Designed server to handle data from websites and stored it onto SQL Server.",
+          "Created automated test scripts and integration of new code using Travis CI.",
+          "Developed DevOps workflow using Docker, Git, Github, and Kubernetes.",
+          "Used Web Services for communicating with other applications and components."
+        ]
+      },
+      {
+        "companyName": "Capgemini",
+        "workImage": "https://firebasestorage.googleapis.com/v0/b/personal-website-f8d5d.appspot.com/o/images%2Fimg-1588187821368?alt=media&token=29724cfe-3433-4cc1-8f1a-8a2f836ba910",
+        "designation": "Software Consultant",
+        "location": "Atlanta, Georgia",
+        "duration": "Jul 2018 - Feb 2021 · 2 yrs 8 mos",
+        "responsibilities": [
+          "Developed and unit tested MoovWeb code to transform existing websites using a routed server.",
+          "Created HTML/CSS3 & JavaScript code to apply new/existing artifacts to transformations and deploy on MoovWeb Cloud.",
+          "Developed RESTful microservices using Spring Boot and Java 8.",
+          "Migrated the database from SQL to MongoDB, improving average response time by 140%.",
+          "Modernized deployment process by automating with GitHub, Jenkins, UrbanCode Deploy and Pivotal Cloud Foundry.",
+          "Created SPA for accelerators using Angular 9 with custom directives and dependency injection.",
+          "Generated reusable modules/assets to be used with multiple modules across multiple applications."
+        ]
+      },
+      {
+        "companyName": "OneTrust",
+        "workImage": "https://130e178e8f8ba617604b-8aedd782b7d22cfe0d1146da69a52436.ssl.cf1.rackcdn.com/onetrust-raises-150m-from-al-gores-firm-after-huge-layoffs-showcase_image-6-a-22632.jpg",
+        "designation": "Software Engineer",
+        "location": "Atlanta, Georgia, United States",
+        "duration": "Feb 2021 - Jul 2022 · 1 yr 6 mos",
+        "responsibilities": [
+          "Developed the design and implementation of User Interaction (UI) flow using HTML5, CSS3, TypeScript, and Angular 8/9.",
+          "Acted as a key mentor to junior and mid-level developers, providing guidance on complex code challenges.",
+          "Contributed significantly to the Agile development process, participating in sprint planning, daily standups, and retrospectives.",
+          "Worked closely with cross-functional teams to develop reusable UI components and ensure consistency across the application.",
+          "Designed and established libraries to support Micro Front-End (MFE) architecture.",
+          "Optimized development workflows and processes, helping establish streamlined feature implementation, testing, and deployment procedures."
+        ]
+      },
+      {
+        "companyName": "Realtor.com",
+        "workImage": "https://static.rdc.moveaws.com/rdc-ui/logos/logo-brand.svg",
+        "designation": "Staff Software Engineer",
+        "location": "Atlanta, Georgia, United States",
+        "duration": "Aug 2023 - Present · 1 yr 8 mos",
+        "responsibilities": [
+          "Lead the design and development of scalable web applications using React, Node.js, TypeScript, and GraphQL.",
+          "Utilize Next.js for SSR and SSG to enhance performance, SEO, and load times.",
+          "Design dynamic UIs with styled-components for consistency across devices and efficient, maintainable code.",
+          "Write and maintain unit tests with Jest, covering components and edge cases, using mocks to validate expected outcomes.",
+          "Collaborate on building scalable backend infrastructure with Node.js, GraphQL, and AWS AuroraDB.",
+          "Implement infrastructure-as-code with AWS CloudFormation.",
+          "Maintain CI/CD pipelines using Jenkins and CircleCI.",
+          "Serve as a Certified SAFe® 5 Scrum Master, leading Agile Scrum processes.",
+          "Monitor system performance using Google Core Web Vitals and New Relic to optimize load times and user experience.",
+          "Mentor junior engineers, supporting all stages of development from design to deployment."
+        ]
+      }
+    ]
+  }
+  
+  module.exports = work_experience;
+  
